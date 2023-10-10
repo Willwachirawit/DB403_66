18badd3885c3a33f4d7f9935199284a63e26bcd5
Willwachirawit <willwachirawit@gmail.com>
2023-10-10 12:04:11 +0700

    lab123

--- student_activity/signup.php
+++ student_activity/signup.php
@@ -1,5 +1,17 @@
 <?php
     require 'connect.php';
+    if (isset($_POST['submit'])) {
+      $studentID = $_POST['studentID'];
+      $studentName = $_POST['studentName'];
+      $majorID = $_POST['major'];
+      $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+      $sql = "insert into 
+      student(studentID, studentName, majorID, password)
+        values('{$studentID}', '{$studentName}',
+               '{$majorID}', '{$password}')";
+               echo $sql;
+        $conn->query($sql);
+    }
 ?>
 <!doctype html>
 <html lang="en">
@@ -23,25 +35,35 @@
         z-index: 2;
       }
 
-      
+
     </style>
+    <script>
+      function validate() {
+        let p1 = document.querySelector('#password').value;
+        let p2 = document.querySelector('#re-password').value;
+        if (p1 != p2) {
+          alert('Password are not identical.');
+          event.preventDefault();
+        }
+      }
+    </script>
   </head>
   <body class="d-flex align-items-center py-4 bg-body-tertiary">
     <main class="form-signup w-100 m-auto">
-      <form>
-        <img class="mb-4" src="images/apple.png" alt="" width="180" height="57">
+      <form action = "signup.php" method="post" onsubmit="validate()">
+        <img class="mb-4" src="images/apple.png" alt="" height="57">
         <h1 class="h3 mb-3 fw-normal">Please sign up</h1>
     
-        <div class="form-floating mb-1">
-          <input type="text" class="form-control" id="student-id" placeholder="Email address">
+        <div class="form-floating mb-2">
+          <input required name ="studentID" type="text" class="form-control" id="student_id" placeholder="">
           <label for="student-id">Student ID</label>
         </div>
-        <div class="form-floating mb-1">
-          <input type="text" class="form-control" id="student-name" placeholder="Email address">
+        <div class="form-floating mb-2">
+          <input required name = "studentName" type="text" class="form-control" id="student_name" placeholder="">
           <label for="student-name">Student Name</label>
         </div>
-        <div class="form-floating mb-1">
-          <select class="form-select" id="major"> 
+        <div class="form-floating mb-2">
+          <select name="major" class="form-control" id="major"> 
     <?php
         $sql = 'select * from major order by faculty';
         $result = $conn->query($sql);
@@ -52,18 +74,18 @@
         $conn->close();
     ?>
           </select>
-          <label for="major">Major</label>
+          <label for="major">MajorID</label>
         </div>
         <div class="form-floating mb-1">
-          <input type="password" class="form-control" id="password" placeholder="Email address">
+          <input required name = "password" type="password" class="form-control" id="password" placeholder="">
           <label for="password">Password</label>
         </div>
         <div class="form-floating mb-3">
-          <input type="password" class="form-control" id="re-password" placeholder="Password">
+          <input required type="password" class="form-control" id="re-password" placeholder="">
           <label for="re-Password">Retype-Password</label>
         </div>
     
-        <button class="btn btn-primary w-100 py-2" type="submit">Sign up</button>
+        <button name = "submit"class="btn btn-primary w-100 py-2" type="submit">Sign up</button>
         <p class="mt-5 mb-3 text-body-secondary">© Wachi 2023</p>
       </form>
     </main>
